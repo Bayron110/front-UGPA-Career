2b47bbe528fd634112d3b8e0a8f94fe91cada80a
Bayron110 <bayron124z@hotmail.com>
2026-03-13 05:12:00 -0500

    Version Cambios Inestable

--- main.js
+++ main.js
@@ -9,12 +9,13 @@ autoUpdater.logger = log;
 let mainWindow;
 
 function createWindow() {
+
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        autoHideMenuBar: false,
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
+            contextIsolation: true
         }
     });
 
@@ -24,22 +25,26 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+
     createWindow();
 
     if (app.isPackaged) {
-        autoUpdater.autoDownload = true;
         autoUpdater.checkForUpdatesAndNotify();
     }
+
 });
 
 autoUpdater.on('download-progress', (progress) => {
+
     if (mainWindow) {
         mainWindow.setProgressBar(progress.percent / 100);
         mainWindow.setTitle(`Descargando actualización ${Math.round(progress.percent)}%`);
     }
+
 });
 
 autoUpdater.on('update-downloaded', () => {
+
     if (mainWindow) mainWindow.setProgressBar(-1);
 
     dialog.showMessageBox({
@@ -48,12 +53,11 @@ autoUpdater.on('update-downloaded', () => {
         title: 'Actualización lista',
         message: 'La nueva versión está lista. ¿Deseas reiniciar ahora?'
     }).then(result => {
+
         if (result.response === 0) {
             autoUpdater.quitAndInstall();
         }
-    });
-});
 
-autoUpdater.checkForUpdates().catch(err => {
-    log.error('Error buscando actualización:', err);
+    });
+
 });
