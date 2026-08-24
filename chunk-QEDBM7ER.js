22af3d02251decc02b1f7b3b9d3fbc0a94a409be
Bayron110 <bayron124z@hotmail.com>
2026-08-24 10:35:18 -0500

    Auto-generated commit

---
+++ chunk-QEDBM7ER.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./chunk-PN26TGAK.js";var e="firebase",o="12.10.0";r(e,o,"app");
--- chunk-XGVQT2NX.js
+++
@@ -1,1 +0,0 @@
-import{U as r}from"./chunk-JCF62H7M.js";var e="firebase",o="12.10.0";r(e,o,"app");
